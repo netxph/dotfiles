72a5287f4ff212cd58970bb79b00a1f93745991a
Marc Vitalis <marc.vitalis@live.com>
2018-03-26 19:27:07 +0800

    add oni config

---
+++ oni-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "oni.useDefaultConfig": false,
+    "oni.loadInitVim": true,
+    "oni.hideMenu": true,
+    "statusbar.enabled": false,
+    "tabs.mode": "hidden",
+    "autoClosingPairs.enabled": false
+}
