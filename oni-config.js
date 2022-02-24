89467856b2066d599c714ff1e127fff6cedc052c
Marc Vitalis <marc.vitalis@live.com>
2022-02-24 11:46:18 +0800

    clean slate

--- oni-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "oni.useDefaultConfig": false,
-    "oni.loadInitVim": true,
-    "oni.hideMenu": true,
-    "statusbar.enabled": false,
-    "tabs.mode": "hidden",
-    "autoClosingPairs.enabled": false
-}
--- vim/ginit.vim
+++
@@ -1,4 +0,0 @@
-Guifont! FiraCode NF:h11
-colorscheme gruvbox
-GuiPopupmenu 0
-GuiTabline 0
