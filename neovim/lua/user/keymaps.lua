a99e8c3468c441f2f6ab1804d1af895163e213b9
Marc Vitalis <marc.vitalis@live.com>
2023-01-17 09:32:53 +0800

    add checking of errors

--- neovim/lua/user/keymaps.lua
+++ neovim/lua/user/keymaps.lua
@@ -64,6 +64,7 @@ keymap("n", "<leader>p", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fq", ":Telescope diagnostics bufnr=0<CR>", opts)
 
 -- Git
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
